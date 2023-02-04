9f58e3a1b232f02ba574e6494bd18a2c67fcd68b
Jordan <jordancarnes260@gmail.com>
2023-02-03 21:41:35 -0600

    Changed File To Population

--- C-Scripts/hello.c
+++ C-Scripts/hello.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
 
 int main() {
- 
-   printf("Hello, World!");
-   return 0;
+  double start_population, end_population, growth_rate;
+  int years = 0;
+
+  printf("Enter the starting population: ");
+  scanf("%lf", &start_population);
+  printf("Enter the end population: ");
+  scanf("%lf", &end_population);
+  printf("Enter the growth rate as a decimal: ");
+  scanf("%lf", &growth_rate);
+
+  while (start_population < end_population) {
+    start_population = start_population * (1 + growth_rate);
+    years++;
+  }
+
+  printf("It will take %d years to reach the end population.\n", years);
+  return 0;
 }
