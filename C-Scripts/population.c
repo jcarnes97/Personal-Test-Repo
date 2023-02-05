e63baefa29d564c2f8dff26bb5bf11ab74d42d63
Jordan <jordancarnes260@gmail.com>
2023-02-04 21:39:27 -0600

    cleaned up the code

--- C-Scripts/population.c
+++ C-Scripts/population.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 int main(void) {
-// TODO: Prompt for start size
+
 // Declare Global Variabes
 int current_pop;
 int starting_pop;
@@ -15,7 +15,6 @@ int years;
       starting_pop = get_int("Type Starting Population and Press the Enter Key: ");
   }
   while (starting_pop < 9);
-   printf("\n");
 //Do While Promt Loop for Amount Of Years
   do
   {
@@ -23,13 +22,16 @@ int years;
       years = get_int("Type Amount of Years and Press the Enter Key: ");
   }
   while (years < 0);
+  // print starting population and years after prompt
    printf("\n");
    printf("Starting Population: %d\n", starting_pop);
    printf("Years: %d\n", years);
+   // redefine population as starting population
    pop = starting_pop;
-
+ //   int new_pop = (pop + (pop / 3) - (pop / 4)); (calculating New Population) (years is the number of times the equation has looped)
  //   int pop_growth = pop / 3;
  //   int pop_decline = pop / 4;
+ //   loop to calculate ending population
    do
    {
     int new_pop = (pop + (pop / 3) - (pop / 4));
@@ -37,9 +39,11 @@ int years;
     pop = new_pop;
    }
     while(years > 0);
-
+//    printing end population and bottom of code
    printf("End Population: %d\n", pop);
    printf("\n");
+
+// TODO: Prompt for start size
 // TODO: Prompt for end size
 // TODO: Calculate number of years until we reach threshold
 // TODO: Print number of years
