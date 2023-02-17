c803554c6a52e8620c0bcb493fa6f8441b55fde8
Jordan <64877522+jcarnes97@users.noreply.github.com>
2023-02-16 18:32:58 -0600

    Update population.c

--- C-Scripts/population.c
+++ C-Scripts/population.c
@@ -3,24 +3,23 @@
 int main() {
     int start_population, end_population, n_years = 0;
 
-    // Prompt the user for the starting and end populations
     do {
         printf("Enter the starting population (must be greater than 9): ");
         scanf("%d", &start_population);
     } while (start_population <= 9);
 
+    do{
     printf("Enter the end population: ");
     scanf("%d", &end_population);
+    }while (end_population <= start_population);
 
-    // Calculate the number of years it takes to reach the end population from the starting population
     int n = start_population;
     while (n < end_population) {
         n += n/3 - n/4;
         n_years++;
     }
 
-    // Print the result
-    printf("It takes %d years to go from %d to %d.\n", n_years, start_population, end_population);
+    printf("It will take %d years\n", n_years);
 
     return 0;
 }
