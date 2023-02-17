1b329f4758fcfa0476909bbf4ebba43761843348
Jordan <64877522+jcarnes97@users.noreply.github.com>
2023-02-16 18:26:14 -0600

    Update population.c

--- C-Scripts/population.c
+++ C-Scripts/population.c
@@ -1,49 +1,26 @@
-#include <cs50.h>
 #include <stdio.h>
 
-int main(void) {
+int main() {
+    int start_population, end_population, n_years = 0;
 
-// Declare Global Variabes
-int current_pop;
-int starting_pop;
-int pop;
-int years;
-//Do while Promt loop for starting population > 9
-  do
-  {
-      printf("\n");
-      starting_pop = get_int("Type Starting Population and Press the Enter Key: ");
-  }
-  while (starting_pop < 9);
-//Do While Promt Loop for Amount Of Years
-  do
-  {
-      years = get_int("Type Amount of Years and Press the Enter Key: ");
-  }
-  while (years < 0);
-  // print starting population and years after prompt
-   printf("\n");
-   printf("Starting Population: %d\n", starting_pop);
-   printf("Years: %d\n", years);
-   // redefine population as starting population
-   pop = starting_pop;
- //   int new_pop = (pop + (pop / 3) - (pop / 4)); (calculating New Population) (years is the number of times the equation has looped)
- //   int pop_growth = pop / 3;
- //   int pop_decline = pop / 4;
- //   loop to calculate ending population
-   do
-   {
-    int new_pop = (pop + (pop / 3) - (pop / 4));
-    years = (years - 1);
-    pop = new_pop;
-   }
-    while(years > 0);
-//    printing end population and bottom of code
-   printf("End Population: %d\n", pop);
-   printf("\n");
+    // Prompt the user for the starting and end populations
+    do {
+        printf("Enter the starting population (must be greater than 9): ");
+        scanf("%d", &start_population);
+    } while (start_population <= 9);
 
-// TODO: Prompt for start size
-// TODO: Prompt for end size
-// TODO: Calculate number of years until we reach threshold
-// TODO: Print number of years
+    printf("Enter the end population: ");
+    scanf("%d", &end_population);
+
+    // Calculate the number of years it takes to reach the end population from the starting population
+    int n = start_population;
+    while (n < end_population) {
+        n += n/3 - n/4;
+        n_years++;
+    }
+
+    // Print the result
+    printf("It takes %d years to go from %d to %d.\n", n_years, start_population, end_population);
+
+    return 0;
 }
